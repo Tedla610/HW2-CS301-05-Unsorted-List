c096310fbee6308a18ae4270bc3b97c19aa76588
Tedla610 <56671065+Tedla610@users.noreply.github.com>
2023-09-07 10:16:27 -0700

    First

---
+++ HW2-Unsorted-List.cpp
@@ -0,0 +1,12 @@
+// Created by Tedla Boke on 9/7/23.
+// NetId: uq6435
+// Email: tboke@horizon.csueastbay.edu
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout <<"Hello World!" << endl;
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # HW2-CS301-05-Unsorted-List
+I am using CLion editor and compiler.
